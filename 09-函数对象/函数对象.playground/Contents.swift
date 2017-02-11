80d7b1d6a1d15c3d3a8f995212cdad28f5860801
张丁豪 <984382258@qq.com>
2017-02-11 22:39:25 +0800

    Add

---
+++ 09-函数对象/函数对象.playground/Contents.swift
@@ -0,0 +1,60 @@
+//: Playground - noun: a place where people can play
+
+// 1.函数赋值给常量
+func add(a:Int, b:Int) -> Int {
+    return a + b
+}
+
+// 函数作为对象，赋值给常量
+let myAdd = add
+
+var result = myAdd(1, 2)
+
+
+// 2.函数作为返回值
+func minus(a:Int, b:Int) ->Int {
+    return a - b
+}
+
+/*
+ 参数：operationType: String
+ 返回值：(a: Int, b:Int) -> Int 返回一个函数，该函数有两个参数，并且返回一个Int型的返回值
+ */
+func operation (operationType: String) -> (_ a: Int, _ b:Int) -> Int {
+    if operationType == "add" {
+        return add
+    }
+    return minus
+}
+
+// 调用方法1
+operation(operationType: "add")(3,4)
+
+// 调用方法2：把返回的函数赋值给常量returnFunc
+let returnFunc = operation(operationType: "add")
+result = returnFunc(3,4)
+
+
+// 3.函数作为参数
+func largeNumber(a:Int, b:Int) -> Int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+func smallNumber(a:Int, b:Int) -> Int {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+func comparePlus(funcParam:(Int, Int) ->Int,a:Int, b:Int) -> Int {
+    
+    return funcParam(a, b)
+    
+}
+
+comparePlus(funcParam: largeNumber, a: 3, b: 4)
+comparePlus(funcParam: smallNumber, a: 3, b: 4)
